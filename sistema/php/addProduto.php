d3bad5c18dc8fae84092777b1f5e36b5ce366b04
Vinícius Amirat <viniciusamirat39@gmail.com>
2021-04-20 10:21:33 -0300

    Add funcionalidade de nome único do produto

--- sistema/php/addProduto.php
+++ sistema/php/addProduto.php
@@ -7,19 +7,31 @@ $preco = $_POST['preco'];
 $quantiade = $_POST['quantidade'];
 
 try {
-    $comando = $conexao->prepare("INSERT INTO produtos (produto, marca, preco, quantidade) VALUES (:produto, :marca, :preco, :quantidade);");
-    $comando->execute(array(
-        ':produto'=>$produto,
-        ':marca'=>$marca,
-        ':preco'=>$preco,
-        ':quantidade'=>$quantiade
+    $pesquisa = $conexao->prepare("SELECT produto FROM produtos WHERE produto = :produto");
+    $pesquisa->execute(array(
+        ':produto'=>$produto
     ));
 
-    if ($comando->rowCount() == 1){
-        echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+    if ($pesquisa->rowCount() > 0){
+        echo "<script>alert('Insira o nome de um produto que não exista!');history.go(-1);</script>";
+
     } else {
-        echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        $comando = $conexao->prepare("INSERT INTO produtos (produto, marca, preco, quantidade) VALUES (:produto, :marca, :preco, :quantidade);");
+        $comando->execute(array(
+            ':produto'=>$produto,
+            ':marca'=>$marca,
+            ':preco'=>$preco,
+            ':quantidade'=>$quantiade
+        ));
+
+        if ($comando->rowCount() == 1){
+            echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+        } else {
+            echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        }
     }
+
+    
 } catch (PDOException $e){
     echo "Error: " . $e->getMessage();
 }
