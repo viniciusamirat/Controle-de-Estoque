e508cedbfc06782df09b20340bf3a7e101225558
Vinícius Amirat <viniciusamirat39@gmail.com>
2021-04-27 16:37:44 -0300

    Preenchimento do arquivo de exclusão de fornecedor

---
+++ sistema/php/excluirFornecedor.php
@@ -0,0 +1,20 @@
+<?php
+    include_once "conexao.php";
+
+    $id = $_GET['id'];
+
+    try {
+        $exclusao = $conexao->prepare("DELETE FROM fornecedores WHERE id = :id");
+        $exclusao->execute(array(
+            ':id'=>$id
+        ));
+
+        if ($exclusao->rowCount() == 1){
+            echo "<script>alert('Excluido com sucesso!');history.go(-1);</script>";
+        } else {
+            echo "<script>alert('Erro ao excluir registro!');history.go(-1);</script>";
+        }
+    } catch (PDOException $e) {
+        echo "Error: ".$e->getMessage();
+    }
+?>
