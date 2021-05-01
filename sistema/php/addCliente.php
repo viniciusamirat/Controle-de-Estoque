64520b342245f2ce0fa1844a6765d708c87fb561
Vinícius Amirat <viniciusamirat39@gmail.com>
2021-04-30 23:14:40 -0300

    Correção de cpf duplicado

--- sistema/php/addCliente.php
+++ sistema/php/addCliente.php
@@ -7,19 +7,28 @@ $tel = $_POST['tel'];
 $email = $_POST['email'];
 $data = $_POST['data'];
 try {
-    $comando = $conexao->prepare("INSERT INTO clientes (nome, cpf, telefone, email, data_cadastro) VALUES (:nome, :cpf, :tel, :email, :data_cadastro);");
-    $comando->execute(array(
-        ':nome'=>$nome,
-        ':cpf'=>$cpf,
-        ':tel'=>$tel,
-        ':email'=>$email,
-        ':data_cadastro'=>$data
+    $pesquisaCpf = $conexao->prepare("SELECT * FROM clientes WHERE cpf = :cpf");
+    $pesquisaCpf->execute(array(
+        ':cpf'=>$cpf
     ));
 
-    if ($comando->rowCount() == 1){
-        echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+    if ($pesquisaCpf->rowCount() == 1){
+        echo "<script>alert('Este CPF já é cadastrado!');history.go(-1);</script>";
     } else {
-        echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        $comando = $conexao->prepare("INSERT INTO clientes (nome, cpf, telefone, email, data_cadastro) VALUES (:nome, :cpf, :tel, :email, :data_cadastro);");
+        $comando->execute(array(
+            ':nome'=>$nome,
+            ':cpf'=>$cpf,
+            ':tel'=>$tel,
+            ':email'=>$email,
+            ':data_cadastro'=>$data
+        ));
+
+        if ($comando->rowCount() == 1){
+            echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+        } else {
+            echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        }
     }
 } catch (PDOException $e){
     echo "Error: " . $e->getMessage();
--- sistema/php/addVendedor.php
+++ sistema/php/addVendedor.php
@@ -8,19 +8,28 @@ $email = $_POST['email'];
 $data = $_POST['data'];
 
 try {
-    $comando = $conexao->prepare("INSERT INTO vendedores (nome, cpf, telefone, email, data_admissao) VALUES (:nome, :cpf, :tel, :email, :data_admissao);");
-    $comando->execute(array(
-        ':nome'=>$nome,
-        ':cpf'=>$cpf,
-        ':tel'=>$tel,
-        ':email'=>$email,
-        ':data_admissao'=>$data
+    $pesquisaCpf = $conexao->prepare("SELECT * FROM vendedores WHERE cpf = :cpf");
+    $pesquisaCpf->execute(array(
+        ':cpf'=>$cpf
     ));
 
-    if ($comando->rowCount() == 1){
-        echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+    if ($pesquisaCpf->rowCount() == 1){
+        echo "<script>alert('Este CPF já é cadastrado!');history.go(-1);</script>";
     } else {
-        echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        $comando = $conexao->prepare("INSERT INTO vendedores (nome, cpf, telefone, email, data_admissao) VALUES (:nome, :cpf, :tel, :email, :data_admissao);");
+        $comando->execute(array(
+            ':nome'=>$nome,
+            ':cpf'=>$cpf,
+            ':tel'=>$tel,
+            ':email'=>$email,
+            ':data_admissao'=>$data
+        ));
+
+        if ($comando->rowCount() == 1){
+            echo "<script>alert('Gravado com sucesso!!');history.go(-2);</script>";
+        } else {
+            echo "<script>alert('Erro de gravação!!');history.go(-2);</script>;";
+        }
     }
 } catch (PDOException $e){
     echo "Error: " . $e->getMessage();
