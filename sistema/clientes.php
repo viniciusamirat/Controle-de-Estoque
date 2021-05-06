3baaf612d7e455a57fe0caedf1b72bc90db31e87
Vinícius Amirat <viniciusamirat39@gmail.com>
2021-05-05 22:40:53 -0300

    Add sombra no menu

--- sistema/clientes.php
+++ sistema/clientes.php
@@ -24,31 +24,34 @@
 </head>
 <body>
     <div>
-        <div class="right">
-            <button type="button" class="btn btn-danger" onclick="window.location.href='php/sair.php'" name="clientes">Sair</button>
-        </div>
-        <div class="container-fluid centralizar cabecalho">
-            <h1>Clientes</h1>
-        </div>
-        <div class="container-fluid centralizar">
-            <button type="button" class="btn btn-primary" onclick="window.location.href='estoque.php'" name="estoque">Estoque</button>
-            <button type="button" class="btn btn-primary" onclick="window.location.href='vendas.php'" name="vendas">Vendas</button>
-            <button type="button" class="btn btn-primary" onclick="window.location.href='vendedores.php'" name="vendedores">Vendedores</button>
-            <button type="button" class="btn btn-secondary" onclick="window.location.href='clientes.php'" name="clientes">Clientes</button>
-            <button type="button" class="btn btn-primary" onclick="window.location.href='fornecedores.php'" name="fornecedores">Fornecedores</button>
-            <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Mais
-                </button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="formProdutos.php">Adicionar Produto</a>
-                  <a class="dropdown-item" href="formVendas.php">Adicionar Venda</a>
-                  <a class="dropdown-item" href="formVendedores.php">Adicionar Vendedor</a>
-                  <a class="dropdown-item" href="formClientes.php">Adicionar Cliente</a>
-                  <a class="dropdown-item" href="formFornecedores.php">Adicionar Fornecedor</a>
+        <div class="topo">
+            <div class="right">
+                <button type="button" class="btn btn-danger" onclick="window.location.href='php/sair.php'" name="clientes">Sair</button>
+            </div>
+            <div class="container-fluid centralizar cabecalho">
+                <h1>Clientes</h1>
+            </div>
+            <div class="container-fluid centralizar menu">
+                <button type="button" class="btn btn-primary" onclick="window.location.href='estoque.php'" name="estoque">Estoque</button>
+                <button type="button" class="btn btn-primary" onclick="window.location.href='vendas.php'" name="vendas">Vendas</button>
+                <button type="button" class="btn btn-primary" onclick="window.location.href='vendedores.php'" name="vendedores">Vendedores</button>
+                <button type="button" class="btn btn-secondary" onclick="window.location.href='clientes.php'" name="clientes">Clientes</button>
+                <button type="button" class="btn btn-primary" onclick="window.location.href='fornecedores.php'" name="fornecedores">Fornecedores</button>
+                <div class="btn-group" role="group">
+                    <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Mais
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                    <a class="dropdown-item" href="formProdutos.php">Adicionar Produto</a>
+                    <a class="dropdown-item" href="formVendas.php">Adicionar Venda</a>
+                    <a class="dropdown-item" href="formVendedores.php">Adicionar Vendedor</a>
+                    <a class="dropdown-item" href="formClientes.php">Adicionar Cliente</a>
+                    <a class="dropdown-item" href="formFornecedores.php">Adicionar Fornecedor</a>
+                    </div>
                 </div>
             </div>
         </div>
+        
         <div class="tabela">
             <table  class="table table-hover">
                 <thead>
